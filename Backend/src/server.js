337b431c69f233ddf7a92c056d1ac84c37a41606
Nikhilesh <20010ee128@gmail.com>
2025-10-24 18:03:35 +0530

    setting up express server with cors to integrate frontend and backend server and error handling

---
+++ Backend/src/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import sequelize from "./config/db.js";
+import organizationRoutes from "./routes/OrganizationRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
+
+// ✅ Add this line
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Middleware
+app.use(express.json());
+// org Logo
+app.use("/uploads", express.static("uploads"));
+
+// Base Routes
+app.use("/api/organizations", organizationRoutes);
+app.use("/api/users", userRoutes);
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("API is running... 🚀");
+});
+
+// Sync DB + start server
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("✅ Database synced successfully");
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
+    );
+  })
+  .catch((err) => console.error("❌ DB connection error:", err));
